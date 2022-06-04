f604b74a0190c2c14ee1fd74bd9d5d64496db9e2
Lukas Gabi <lukas.gabi@myvision.me>
2022-06-04 03:01:55 +0200

    configure cert-manager

--- services/cert-manager/cert-manager.sh
+++ services/cert-manager/cert-manager.sh
@@ -2,9 +2,4 @@
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
-helm install \
-  cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
-  --create-namespace \
-  --version v1.7.1 \
-  --set installCRDs=true
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.7.1 --set installCRDs=true
