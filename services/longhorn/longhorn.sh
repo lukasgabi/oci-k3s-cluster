636ffb603467b83d7d406eafdcf565c021ee37db
Lukas Gabi <lukas.gabi@myvision.me>
2022-06-04 02:59:31 +0200

    add longhorn

--- services/longhorn/longhorn.sh
+++ services/longhorn/longhorn.sh
@@ -2,6 +2,6 @@
 helm repo add longhorn https://charts.longhorn.io
 helm repo update
 kubectl create namespace longhorn-system
-helm install longhorn longhorn/longhorn --namespace longhorn-system
+helm install -f values.yaml longhorn longhorn/longhorn --namespace longhorn-system
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
 kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
